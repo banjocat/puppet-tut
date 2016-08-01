9cce65d7c3ea95c2e4efd242a6a75cb3208c98f9
Jack Muratore <jackmuratore@gmail.com>
2016-08-01 01:16:19 -0400

    More puppet fun

--- hello.pp
+++ hello.pp
@@ -12,24 +12,44 @@ elsif $time == "night" {
 else  {
     notify { "Unknown message for $person":}
 }
-service { 'puppet':
-    ensure => 'stopped',
-    enable => 'false'
+
+
+file { 'lol':
+    path => '/home/banjocat/tuts/puppet-tut/lol',
+    ensure => 'directory',
+    owner => 'banjocat',
+    group => 'banjocat',
+    mode => '0644',
 }
 
-
-package { 'docker':
-    ensure => 'installed'
+file {'pizza.log':
+    path => '/home/banjocat/tuts/puppet-tut/lol/pizza.log',
+    require => File['lol'],
+    content => "This is the pizza log!
+    It has a log of all the pizzas.
+    There are none here yet.
+    "
+}
+->
+file {'pizza-link':
+    path => '/home/banjocat/tuts/puppet-tut/lol/pizza-link.log',
+    ensure => 'link',
+    target => '/home/banjocat/tuts/puppet-tut/lol',
 }
 
-service { 'docker':
-    ensure => 'running',
-    require => Package['docker']
+user { 'banjocat':
+    ensure => 'present',
+    home => '/home/banjocat',
+    managehome => true,
+    groups => [ 'banjocat', 'adm', 'cdrom', 'sudo', 'dip', 'plugdev', 'lpadmin', 'sambashare', 'docker' ],
+    shell => '/bin/bash',
 }
 
-package { 'apache2':
-    ensure => 'installed'
+exec { 'other.log':
+    command => 'touch other.log',
+    provider => 'shell',
+    cwd => '/home/banjocat/tuts/puppet-tut/lol/',
+    creates => '/home/banjocat/tuts/puppet-tut/lol/other.log',
+    require => File['lol']
 }
-
-
-
+    
